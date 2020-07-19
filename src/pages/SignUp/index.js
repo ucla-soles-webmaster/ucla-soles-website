551cc40d790a722c603963fcebe378ad09fa79e2
Mat Ruiz <matruizofficial@gmail.com>
2020-07-18 21:56:35 -0400

    Committing before working on parallax

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -95,6 +98,18 @@ class SignUpFormBase extends Component {
 
         return (
             <form onSubmit={this.onSubmit}>
+
+                {/* Items needed for sign up */}
+
+                <Form.Group controlId="formSignUpCareer">
+                  <ButtonGroup aria-label="Career">
+                    <Button name="career" value="UGRAD" variant="secondary" size="sm"  onClick={this.onChange} type="button">Undergraduate</Button>
+                    <Button name="career" value="GRAD" variant="secondary" size="sm" onClick={this.onChange} type="button">Graduate</Button>
+                    <Button name="career" value="ALUM" variant="secondary" size="sm" onClick={this.onChange} type="button">Alumni</Button>
+                    <Button name="career" value="IND" variant="secondary" size="sm" onClick={this.onChange} type="button">Industry</Button>
+                  </ButtonGroup>
+                </Form.Group>
+
                 <input
                     name="username"
                     value={username}
@@ -102,6 +117,7 @@ class SignUpFormBase extends Component {
                     type="text"
                     placeholder="Full Name"
                 />
+                <br/>
                 <input
                     name="email"
                     value={email}
@@ -109,6 +125,7 @@ class SignUpFormBase extends Component {
                     type="text"
                     placeholder="Email Address"
                 />
+                <br/>
                 <input
                     name="passwordOne"
                     value={passwordOne}
@@ -116,6 +133,7 @@ class SignUpFormBase extends Component {
                     type="password"
                     placeholder="Password"
                 />
+                <br/>
                 <input
                     name="passwordTwo"
                     value={passwordTwo}
@@ -123,6 +141,9 @@ class SignUpFormBase extends Component {
                     type="password"
                     placeholder="Confirm Password"
                 />
+                <br/>
+
+
         <button disabled={isInvalid} type="submit">
             Sign Up
         </button>
