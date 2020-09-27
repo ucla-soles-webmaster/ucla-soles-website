c40f594f2d712f2d667edbae1c441362371763c8
sarahmauricio <sarahmauricio2019@gmail.com>
2020-09-26 17:41:10 -0700

    update Points and add Team

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -5,10 +5,12 @@ import AccountNav from '../../components/AccountNav';
 import Footer from '../../components/Footer';
 import STARLeaderboard from '../../components/STARLeaderboard';
 import PasswordChangeForm from '../PasswordChange';
+import FlatList from 'flatlist-react';
 
 import { AuthUserContext, withAuthorization } from '../Session';
 
 import './accountStyle.css'
+
 
 
 class AccountPage extends Component {
@@ -17,8 +19,12 @@ class AccountPage extends Component {
     
     this.state = {
       user: {},
+      userList: [],
+      userIDList: [],
       firestore: this.props.firebase.getFirestore(),
       userEmail: this.props.firebase.auth.currentUser.email,
+      mentorTeam: "",
+      points: 0,
     }
   }
 
@@ -33,7 +39,98 @@ class AccountPage extends Component {
           that.setState({ user: doc.data() })
         });
       })
+
+      var that = this;
+      that.props.firebase.getFirestore().collection("users")
+      .where("email", "==", this.state.userEmail)
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          console.log(doc.id, " => ", doc.data());
+          that.setState({ user: doc.data() })
+        });   
+    });
+
+      this.props.firebase.getFirestore().collection("users")
+        .get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                var userData = doc.data();
+                var userID = doc.id;
+                that.setState({ userList: [...that.state.userList, userData] });
+                that.setState({ userIDList: [...that.state.userIDList, userID] });
+            });    
+        });
   }
+
+  updateTeam (name){
+    if(name!= null)
+      this.setState({mentorTeam: name});
+  }
+
+  changeTeam = (fName, lName, index) => {
+    if(index == null)
+      return;
+    console.log(index);
+    var document = index;
+    var dbReference = this.props.firebase.getFirestore().collection('users').doc(document);
+    console.log(this.state.mentorTeam);
+    dbReference.update({mentorTeam: this.state.mentorTeam})
+
+    var name = fName+ " " + lName;
+    dbReference = this.props.firebase.getFirestore().collection('teams').doc(this.state.mentorTeam).collection('teamMembers').doc(name).set({});
+
+  }
+
+  updatePoints(points){
+    if(points!= 0)
+      this.state.updatePoints = points;
+  }
+
+  changePoints(index, mentorTeamName){
+    var that = this;
+    
+      
+    
+      if(mentorTeamName != null){
+        var dbReference = that.props.firebase.getFirestore().collection('teams').doc(mentorTeamName);
+        console.log(that.state.updatePoints);
+        var points = that.props.firebase.getFirestore().collection('teams').doc(mentorTeamName);
+        points.get().then(function(doc){
+          var currPoints = doc.data();
+          var add = parseInt(currPoints["starpoints"]) + parseInt(that.state.updatePoints);
+          dbReference.update({starpoints: add});
+        });
+        
+      }
+  
+    
+    
+  }
+
+  renderUser = (user, idx) => {
+    return (
+      <div class="adminUserCell">
+
+        {/* User name */}
+        <div class="adminUserName">
+          { user["first_name"] } { user["last_name"] }
+        </div>    
+
+        <input type = "text" placeholder="Team Name" id="mentorTeamName" onChange={e=> this.updateTeam(e.target.value)}/>
+       
+        <input type ="submit" className = "btn btn-info" value = "Change Team" onClick={()=>this.changeTeam(user["first_name"], user["last_name"],this.state.userIDList[idx])}></input>
+      
+        <input type = "number" placeholder="Points" id="points" onChange={e=> this.updatePoints(e.target.value)}/>
+        {console.log(this.state.updatePoints)}
+        <input type ="submit" className = "btn btn-info" value = "Add Points" onClick={()=>this.changePoints(this.state.userIDList[idx], user["mentorTeam"])}></input>
+
+      </div>
+    );
+  }
+
+  
 
   render() {
     return (
@@ -60,6 +157,20 @@ class AccountPage extends Component {
               </div>
             )}
           </AuthUserContext.Consumer>
+
+          <div class="adminUsersList">
+            { this.state.user["admin"] === true ?
+              <FlatList
+              list={this.state.userList}
+              renderItem={this.renderUser}
+              />
+                    :
+                      <div>
+                        <p>No Access</p>
+                      </div>
+              }
+            </div>
+
 
 
         </div>
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,17 +3,76 @@ import React, { Component } from 'react';
 import Navigation from '../../components/Navigation';
 import PagePhoto from '../../components/PagePhoto';
 import coverPhoto from '../../coverImages/contact.JPG';
+import emailjs from 'emailjs-com';
+import './contact.css';
 
+
+import { Button, FormFeedback, Form, FormGroup, Label, Input } from 'reactstrap'
+
+function sendEmail(e){
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_d19ecvb', e.target, 'user_2CrH6Hv9biqjQoD3JEFWe')
+        .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log("hello");
+      console.log(error.text);
+    });
+    e.target.reset();
+}
 
 class Contact extends Component {
     
     // add constructor if necessary
+    
 
     render() {
         return (
             <div>
                 <Navigation transparentNav={true} />
                 <PagePhoto imageSource={coverPhoto} title={"Contact Us"}></PagePhoto>
+                <h1 className = "contactTitle">Have any questions? Send them our way!</h1>
+
+
+                <div className = "contactForm">
+                    <form onSubmit={sendEmail}>
+                        <div>
+                            <div className = "contactComponenet">
+                                <input type= "text" placeholder = "Name" name = "name"/>
+                            </div>
+                            <div className = "contactComponenet">
+                                <input type = "email" placeholder= "Email Address" name = "email"/>
+                            </div>
+                            <div className = "contactComponenet">
+                                <input type = "text" placeholder="Subject" name = 'subject'/>
+                            </div>
+                            <div className = "contactComponenet">
+                                <textarea id="" cols="30" rows="8" placeholder="Your Message" name = "message"></textarea>
+                            </div>
+                            <div className = "contactComponenet">
+                                <input type ="submit" className = "btn btn-info" value = "Send Message"></input>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+
+
+
+
+
+
+                <div style = {{textAlign: 'center'}}>
+                    <h1 className = "contactTitle">Follow us on social media!</h1>
+                    <a  href="https://www.facebook.com/uclasoles/ " rel="noopener noreferrer" target = "_blank" >
+                        <i style = {{padding: '1vw',fontSize: '3vw', color: 'black'}} class="fa fa-facebook-square" aria-hidden="true"></i>
+                    </a>
+
+                    <a href="https://www.instagram.com/uclasoles/?hl=en" rel="noopener noreferrer" target = "_blank" >
+                        <i style = {{padding: '1vw', fontSize: '3vw', color: 'black'}} class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                </div>
                 <br/>
                 <br/>
                 <br/>
