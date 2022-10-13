70ab36790571c70a908958b76e885b8f10449519
Mat Ruiz <mat@Mats-MacBook-Air.local>
2022-10-12 21:06:04 -0700

    Added fall drop coming soon to merch shop carosel

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -31,11 +31,11 @@ import sun from '../../Photos/Icons/sunblue.png';
 // NOTE: for Carousel, images can only be links (not saved on disk) to images, thats why we upload to our Firebase or another website
     const data = [
         {
-            image: 'https://firebasestorage.googleapis.com/v0/b/soles-website-dev.appspot.com/o/resumes%2FSpring-Drop-Banner.png?alt=media&token=85a6d215-5141-4587-a09a-3fb7f0fa9ca2'
+            image: 'https://firebasestorage.googleapis.com/v0/b/soles-website-dev.appspot.com/o/resumes%2Ffalldrop2022.png?alt=media&token=eb85fa16-89de-42a7-93d6-a425e03aeb5e'
         },
         {
-            image: "https://firebasestorage.googleapis.com/v0/b/soles-website-dev.appspot.com/o/resumes%2FSOLES%20Membership%20Shirts.png?alt=media&token=29edfbb7-9d0b-42a9-b7af-3214a8fb75f1"
-        },
+            image: 'https://firebasestorage.googleapis.com/v0/b/soles-website-dev.appspot.com/o/resumes%2FSpring-Drop-Banner.png?alt=media&token=85a6d215-5141-4587-a09a-3fb7f0fa9ca2'
+        }
     ];
     const captionStyle = {
         fontSize: '2em',
